81d1bf46b857748fe0a6710a6d924202f0a4f3f6
KirimLee <kirim@sk.com>
2021-12-02 23:38:58 +0900

    :recycle: devide and conquer

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,27 @@
-import { PrismaClient } from '@prisma/client';
-import { ApolloServer, gql } from 'apollo-server';
+import { ApolloServer } from 'apollo-server';
 import {
   ApolloServerPluginLandingPageGraphQLPlayground,
   ApolloServerPluginLandingPageDisabled,
 } from 'apollo-server-core';
+import { mergeTypeDefs, mergeResolvers } from '@graphql-tools/merge';
+import { loadFilesSync } from '@graphql-tools/load-files';
+import path from 'path';
+import { makeExecutableSchema } from '@graphql-tools/schema';
 
-const client = new PrismaClient();
+const typesArray = mergeTypeDefs(
+  loadFilesSync(`${__dirname}/**/*.typeDefs.ts`)
+);
+const resolversArray = mergeResolvers(
+  loadFilesSync(path.join(__dirname, '**/*.{queries,mutations}.ts')) as any
+);
 
-const typeDefs = gql`
-  type Movie {
-    id: Int!
-    title: String!
-    year: Int!
-    genre: String
-    createAt: String!
-    updateAt: String!
-  }
-  type Query {
-    movies: [Movie]
-    movie(id: Int!): Movie
-  }
-  type Mutation {
-    createMovie(title: String!, year: Int!, genre: String): Movie
-    deleteMovie(id: Int): Movie
-    updateMovie(id: Int, year: Int!): Movie
-  }
-`;
-
-const resolvers = {
-  Query: {
-    movies: () => client.movie.findMany(),
-    movie: (_: any, { id }: { id: number }) =>
-      client.movie.findUnique({ where: { id } }),
-  },
-  Mutation: {
-    createMovie: (_, { title, year, genre }) =>
-      client.movie.create({ data: { title, year, genre } }),
-    deleteMovie: (_, { id }) => client.movie.delete({ where: { id } }),
-    updateMovie: (_, { id, year }) =>
-      client.movie.update({ where: { id }, data: { year } }),
-  },
-};
+const schema = makeExecutableSchema({
+  typeDefs: typesArray,
+  resolvers: resolversArray,
+});
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  schema,
   plugins: [
     process.env.NODE_ENV === 'production'
       ? ApolloServerPluginLandingPageDisabled()
