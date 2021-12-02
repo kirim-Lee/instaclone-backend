f84c1410aa3df17d72f66fee3ea13511d6822e10
KirimLee <kirim@sk.com>
2021-12-02 19:21:12 +0900

    :sparkles: sample code and add run script

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,56 @@
+import { PrismaClient } from '@prisma/client';
 import { ApolloServer, gql } from 'apollo-server';
+import {
+  ApolloServerPluginLandingPageGraphQLPlayground,
+  ApolloServerPluginLandingPageDisabled,
+} from 'apollo-server-core';
+
+const client = new PrismaClient();
 
 const typeDefs = gql`
-  type Book {
-    title: String
-    author: String
+  type Movie {
+    id: Int!
+    title: String!
+    year: Int!
+    genre: String
+    createAt: String!
+    updateAt: String!
   }
   type Query {
-    books: [Book]
-    book(title: String): Book
+    movies: [Movie]
+    movie(id: Int!): Movie
+  }
+  type Mutation {
+    createMovie(title: String!, year: Int!, genre: String): Movie
+    deleteMovie(id: Int): Movie
+    updateMovie(id: Int, year: Int!): Movie
   }
 `;
 
-const books = [
-  {
-    title: 'The Awakening',
-    author: 'Kate Chopin',
-  },
-  {
-    title: 'City of Glass',
-    author: 'Paul Auster',
-  },
-];
-
 const resolvers = {
   Query: {
-    books: () => books,
-    book: (_: any, { title }: { title: string }) => {
-      return books.find((book) => book.title === title);
-    },
+    movies: () => client.movie.findMany(),
+    movie: (_: any, { id }: { id: number }) =>
+      client.movie.findUnique({ where: { id } }),
+  },
+  Mutation: {
+    createMovie: (_, { title, year, genre }) =>
+      client.movie.create({ data: { title, year, genre } }),
+    deleteMovie: (_, { id }) => client.movie.delete({ where: { id } }),
+    updateMovie: (_, { id, year }) =>
+      client.movie.update({ where: { id }, data: { year } }),
   },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [
+    process.env.NODE_ENV === 'production'
+      ? ApolloServerPluginLandingPageDisabled()
+      : ApolloServerPluginLandingPageGraphQLPlayground(),
+  ],
+});
 
 server.listen().then(({ url }: { url: string }) => {
   console.log(`🚀  Server ready at ${url}`);
