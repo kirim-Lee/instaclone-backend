4b35fc3b421697b8e3b4dd1c4c0e83eb13ed8dc6
KirimLee <kirim@sk.com>
2021-12-03 22:42:22 +0900

    :sparkles: view profile

--- src/users/users.queries.ts
+++ src/users/users.queries.ts
@@ -1,3 +1,13 @@
+import { Prisma, User } from '@prisma/client';
+import client from '../client';
+
 export default {
-  Query: {},
+  Query: {
+    seeProfile: async (
+      _: never,
+      { username }: Pick<Prisma.UserWhereUniqueInput, 'username'>
+    ): Promise<User | null> => {
+      return await client.user.findUnique({ where: { username } });
+    },
+  },
 };
